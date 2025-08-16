8f83983070781731c2d3e34c63067b137651eb43
Rhythm Mondal <mondal.rhythm.rm64@gmail.com>
2025-08-16 21:52:38 +0600

    - added CTRL_KEY macro - ctrl+q now quits editor

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios default_termios;
 
 void die(const char *s){
@@ -39,7 +41,7 @@ int main(){
         if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if(iscntrl(c)) printf("%d\r\n", c);
         else printf("%d ('%c')\r\n", c, c);
-        if (c=='q') break;
+        if (c==CTRL_KEY('q')) break;
     }
     return 0;
 }
